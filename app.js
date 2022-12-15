58f4df47488fa57609bed10e46ca48d9a93d66e6
Aulugbekov <119114287+Aulugbekov@users.noreply.github.com>
2022-12-15 11:51:38 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+for (let i = 0; i <= 20; i++) {
+  if (i % 2) {
+  } else {
+    console.log([i]);
+  }
+}
+
+var blacklist = [
+  "baiaz",
+  "jamaldin",
+  "ali",
+  "dastan",
+  "Baiaz",
+  "Jamaldin",
+  "Ali",
+  "Dastan",
+  "BAIAZ",
+  "JAMALDIN",
+  "ALI",
+  "DASTAN",
+];
+
+var name = prompt("type ur name:");
+
+do {
+  if (blacklist.includes(name)) {
+    name = prompt("Type your name:");
+  } else {
+    alert(`Welcom ${name}`);
+    break;
+  }
+} while (true);
